5d8717823cf4d32c721c6b7c4733e55a0f0d5296
Edgar Latorre <me@edgarlatorre.com.br>
2024-11-07 10:53:06 +0100

    Change caps to escape

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -6,3 +6,6 @@ gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Ner
 
 # Reveal week numbers in the Gnome calendar
 gsettings set org.gnome.desktop.calendar show-weekdate true
+
+# Remape caps to escape
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
