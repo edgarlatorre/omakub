71b47b545531f9d7ba73bb4b60f1ade60fc9b7a1
Edgar Latorre <me@edgarlatorre.com.br>
2024-11-15 13:40:01 +0100

    Add default config for zellij layout

--- install/terminal/app-zellij.sh
+++ install/terminal/app-zellij.sh
@@ -1,5 +1,6 @@
 yay -S --needed zellij --noconfirm
 
-mkdir -p ~/.config/zellij/themes
+mkdir -p ~/.config/zellij/{themes,layouts}
 [ ! -f "$HOME/.config/zellij/config.kdl" ] && cp ~/.local/share/omakub/configs/zellij.kdl ~/.config/zellij/config.kdl
+[ ! -f "$HOME/.config/zellij/layouts/bla.kdl" ] && cp ~/.local/share/omakub/configs/zellij_layout.kdl ~/.config/zellij/layouts/default.kdl
 cp ~/.local/share/omakub/themes/tokyo-night/zellij.kdl ~/.config/zellij/themes/tokyo-night.kdl
